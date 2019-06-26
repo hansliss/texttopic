222bc86011a67d1c3182dc0fa56baceef5236fb9
Hans Liss <Hans.Liss@uadm.uu.se>
2019-06-26 12:27:24 +0200

    More work on texttofont

--- texttofont.c
+++ texttofont.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <float.h>
 #include <math.h>
+#include <types.h>
+#include <stdint.h>
 
 #define FONTSIZE 512
 #define FONTGEOMX 8
@@ -12,6 +14,50 @@
 
 void usage(char *progname) {
   fprintf(stderr, "Usage: %s -F <font file> -C <charset file> -o <output file>\n", progname);
+}
+
+typedef struct { // Data stored PER GLYPH
+  uint16_t bitmapOffset;     // Pointer into GFXfont->bitmap
+  uint8_t  width, height;    // Bitmap dimensions in pixels
+  uint8_t  xAdvance;         // Distance to advance cursor (x axis)
+  int8_t   xOffset, yOffset; // Dist from cursor pos to UL corner
+} GFXglyph;
+
+typedef struct { // Data stored for FONT AS A WHOLE:
+  uint8_t  *bitmap;      // Glyph bitmaps, concatenated
+  GFXglyph *glyph;       // Glyph array
+  uint8_t   first, last; // ASCII extents
+  uint8_t   yAdvance;    // Newline distance (y axis)
+} GFXfont;
+
+typedef struct _fontinfo {
+  unsigned char character;
+  uint8_t *bitmap;
+  uint8_t width, height, xAdvance, xOffset, yOffset;
+  struct _fontinfo *next;
+} *fontinfo;
+
+void addFi(fontinfo *fi, unsigned char c, uint8_t width, uint8_t height, uint8_t xAdvance, uint8_t xOffset, uint8_t yOffset) {
+  if (*fi && ((*fi)->character < c)) {
+    addFi(&((*fi)->next), width, height, xAdvance, xOffset, yOffset);
+  } else {
+    fontinfo tmp = (fontinfo)malloc(sizeof(struct _fontinfo));
+    tmp->width = width;
+    tmp->height = height;
+    tmp->xAdvance = xAdvance;
+    tmp->xOffset = xOffset;
+    tmp->yOffset = yOffset;
+    tmp->next = *fi;
+    *fi = tmp;
+  }
+}
+
+fontinfo findFi(fontinfo fi, unsigned char c) {
+  if (fi && (fi->character != c)) {
+    return findFi(fi->next, c);
+  } else {
+    return fi;
+  }
 }
 
 typedef struct font_s {
@@ -33,6 +79,35 @@ int findBits(font f, unsigned char c) {
     return i * FONTGEOMY;
   } else {
     return -1;
+  }
+}
+
+int checkColumn(font f, int fBitsIndex, int column) {
+  int i;
+  for (i=0; i<FONTGEOMY; i++) {
+    if (f[fBitsIndex + i] & (1 << (7 - column))) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+void findlimits(font f, int fBitsIndex, int &l, int &r, int &t, int &b) {
+  l=0;
+  r=FONTGEOMX-1;
+  t=0;
+  b=FONTGEOMY-1;
+  while ((t < FONTGEOMY) && !f[fBitsIndex + t]) {
+    t++;
+  }
+  while ((b >= t) && !f[fBitsIndex + b]) {
+    b--;
+  }
+  while ((l < FONTGEOMX) && !checkColumn(f, fBitsIndex, l)) {
+    l++;
+  }
+  while ((r >= l) && !checkColumn(f, fBitsIndex, r))) {
+    r--;
   }
 }
 
